e56c5c7832983a15a0f29425235bb9036c883648
ser6nah <70658189+ser6nah@users.noreply.github.com>
2020-10-07 20:00:13 -0400

    Add files via upload

---
+++ src/Project/Project.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Project.css";
+import { Link } from "react-router-dom";
+
+export default function Project(props) {
+  return (
+    <Link
+      to={{ pathname: props.link }}
+      className="project-background"
+      target="_blank"
+    >
+      <div className="project-image"> 
+        <img className="project-image" src={props.source}/>
+      </div>
+      <div className="project-info">
+        <h1>{props.title}</h1>
+        <p>{props.description}</p>
+      </div>
+    </Link>
+  );
+}
